f2c0e75acefccf584367a96b6fc2d6644ba3bd15
nithish-007 <itsmedecoder07@gmail.com>
2025-01-27 23:59:03 +0530

    partial resnet-50

---
+++ resnet-50.py
@@ -0,0 +1,50 @@
+import torch 
+from torch import nn
+
+class block(nn.Module):
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super(block, self).__init__()
+        self.expansion = 4
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
+        self.relu = nn.ReLU()
+        self.identity_downsample = identity_downsample
+    
+    def forward(self, x):
+        identity = x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+        return x
+
+class ResNet(nn.Module): #[3, 4, 6, 3]
+    def __init__(self, block, layers, image_channels, num_classes):
+       super(ResNet, self).__init__()
+       self.in_channels = 64
+       self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+       self.bn1 = nn.BatchNorm2d(64)
+       self.relu = nn.ReLU()
+       self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+       
+
+
+
+
